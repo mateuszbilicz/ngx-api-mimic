5bcc396ed558ee97ba14a105465ec9dc5c6f7a3a
Mateusz Bilicz <mateuszbilicz@gmail.com>
2026-03-05 09:11:37 +0100

    working on DOCS app added icons filled WelcomeViewComponent changed NavigationComponent changed index.html added llms.txt added robots.txt added sitemap.xml

--- src/app/views/welcome-view/welcome-view.component.ts
+++ src/app/views/welcome-view/welcome-view.component.ts
@@ -1,10 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import packageJson from '../../../../projects/ngx-api-mimic/package.json';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-welcome-view',
-  imports: [],
+  imports: [CardModule, ButtonModule, DividerModule, ChipModule],
   templateUrl: './welcome-view.component.html',
   styleUrl: './welcome-view.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WelcomeViewComponent {}
+export class WelcomeViewComponent {
+  version = signal(packageJson.version);
+  libName = signal(packageJson.name);
+  description = signal(packageJson.description);
+  authorName = signal(packageJson.author.name);
+  authorUrl = signal(packageJson.author.url);
+  authorGithubUrl = signal('https://github.com/mateuszbilicz');
+  githubUrl = signal(packageJson.repository);
+
+  features = signal([
+    'Mock data and REST API simultaneously using @UsingSchema on Controllers',
+    'Intelligent schema-based mocked data generation via TypeScript interfaces',
+    'Automated mocked data storage in the browser\'s LocalStorage',
+    'Complete mock backend features: angular interceptor, router, controllers, decorators, and guards',
+    'Easily migrate mocked API to NestJS thanks to identical API decorators and architecture'
+  ]);
+}
